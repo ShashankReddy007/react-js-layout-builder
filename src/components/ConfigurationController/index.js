8bd7b7de4b8c429280219907ce1021a5327ab4e8
Shashank Reddy <shashank.godala@gmail.com>
2023-03-30 22:18:31 -0700

    second-commit

--- src/components/ConfigurationController/index.js
+++ src/components/ConfigurationController/index.js
@@ -1,1 +1,64 @@
 // Write your code here
+import ConfigurationContext from '../../context/ConfigurationContext'
+import './index.css'
+
+const ConfigurationController = () => (
+  <ConfigurationContext.Consumer>
+    {value => {
+      const {
+        showContent,
+        showLeftNavbar,
+        showRightNavbar,
+        onToggleShowContent,
+        onToggleShowLeftNavbar,
+        onToggleShowRightNavbar,
+      } = value
+
+      const onCheckedShowContent = () => {
+        onToggleShowContent()
+      }
+
+      const onCheckedShowLeftContent = () => {
+        onToggleShowLeftNavbar()
+      }
+
+      const onCheckedShowRightContent = () => {
+        onToggleShowRightNavbar()
+      }
+
+      return (
+        <nav className="nav-header">
+          <h1>Layout</h1>
+          <label>
+            <input
+              type="checkbox"
+              checked={showContent}
+              onChange={onCheckedShowContent}
+            />
+            Content
+          </label>
+
+          <label>
+            <input
+              type="checkbox"
+              checked={showLeftNavbar}
+              onChange={onCheckedShowLeftContent}
+            />
+            Left NavBar
+          </label>
+
+          <label>
+            <input
+              type="checkbox"
+              checked={showRightNavbar}
+              onChange={onCheckedShowRightContent}
+            />
+            Right NavBar
+          </label>
+        </nav>
+      )
+    }}
+  </ConfigurationContext.Consumer>
+)
+
+export default ConfigurationController
